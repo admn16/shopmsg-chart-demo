90aa9d15bbf1f88ecfd2cf336db4862c1a3604c7
Anton Nocum <anton.nocum@gmail.com>
2019-05-14 22:10:27 +0800

    Declared and moved all styles under styles variable

--- client/components/DashboardApp.tsx
+++ client/components/DashboardApp.tsx
@@ -1,24 +1,36 @@
 "use strict";
 import * as React from 'react';
-import { Layout, Menu, Breadcrumb, Icon, Card } from 'antd';
+import {
+  Layout,
+  Menu,
+  Breadcrumb,
+  Icon,
+  Card
+} from 'antd';
+import InputForm from './InputForm'
 
-const { Header, Content, Footer, Sider } = Layout;
+const {
+  Header,
+  Content,
+  Footer,
+  Sider
+} = Layout;
 const SubMenu = Menu.SubMenu;
 
 require('antd/dist/antd.less');
-export default class DashboardApp extends React.Component {
+export default class DashboardApp extends React.Component <{}> {
   state = {
     collapsed: false,
   };
 
-  onCollapse = (collapsed) => {
+  onCollapse = (collapsed: boolean) => {
     console.log(collapsed);
     this.setState({ collapsed });
   }
 
   render() {
     return (
-      <Layout style={{ minHeight: '100vh' }}>
+      <Layout style={styles.layout}>
         <Sider
           collapsible
           collapsed={this.state.collapsed}
@@ -56,17 +68,39 @@ export default class DashboardApp extends React.Component {
           </Menu>
         </Sider>
         <Layout>
-          <Header style={{ background: '#fff', padding: 0 }} />
-          <Content style={{ margin: '0 16px' }}>
-            <Breadcrumb style={{ margin: '16px 0' }}>
+          <Header style={styles.header}>
+            Reports
+          </Header>
+          <Content style={styles.content}>
+            <Breadcrumb style={styles.breadcrumb}>
               <Breadcrumb.Item>Reports</Breadcrumb.Item>
               <Breadcrumb.Item>Message Receipts & Optins</Breadcrumb.Item>
             </Breadcrumb>
-            <Card>Put your component here</Card>
+            <Card>
+              <InputForm/>
+            </Card>
           </Content>
           <Footer style={{ textAlign: 'center' }}>ShopMessage ©2018</Footer>
         </Layout>
       </Layout>
     );
+  }
+}
+
+const styles = {
+  breadcrumb: {
+    margin: '16px 0'
+  },
+  content: {
+    margin: '0 16px'
+  },
+  header: {
+    background: '#fff',
+    fontSize: '24px',
+    fontWeight: 500,
+    padding: '0 16px'
+  },
+  layout: {
+    minHeight: '100vh'
   }
 }
